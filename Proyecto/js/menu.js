8f72ace5ccdd5768ccbaf8df38360bd6f4013cb5
Jesuslperezc <jesusleonardoperezcabrera@gmail.com>
2026-06-07 16:33:52 -0400

    feat: realizar funciones para que sirva el menu

---
+++ Proyecto/js/menu.js
@@ -0,0 +1,33 @@
+let modoJuego="";
+
+document.addEventListener("DOMContentLoaded",() => {
+    const botonesMenu = document.querySelectorAll(".boton-menu");
+    var botonRegresar = document.querySelectorAll(".boton-back");
+    let botonTema = document.querySelector(".boton-menu-tema");
+    botonesMenu.forEach(boton => {
+        boton.addEventListener("click", (evento) => {
+            modoJuego = evento.currentTarget.dataset.mode;
+            console.log("Modo de juego seleccionado:", modoJuego);
+            document.getElementById("main-menu").classList.add("oculto");
+            document.getElementById("selection-menu").classList.remove("oculto");
+        });
+        
+    });
+    if (botonTema) {
+            botonTema.addEventListener("click", (evento) => {
+            document.getElementById("main-menu").classList.add("oculto");
+            document.getElementById("theme-menu").classList.remove("oculto");
+            });
+    }
+
+    botonRegresar.forEach(boton => {
+         boton.addEventListener("click", (evento) => {
+            console.log("Regresando al menú principal");
+            document.getElementById("selection-menu").classList.add("oculto");
+            document.getElementById("theme-menu").classList.add("oculto");
+            document.getElementById("main-menu").classList.remove("oculto");
+        });
+    });
+
+});
+
