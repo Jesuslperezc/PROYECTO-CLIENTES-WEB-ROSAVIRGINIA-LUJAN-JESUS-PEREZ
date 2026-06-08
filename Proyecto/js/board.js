c166cd70a9b1925fff824c70014c184ed27f143b
Jesuslperezc <jesusleonardoperezcabrera@gmail.com>
2026-06-08 14:03:06 -0400

    feat:  memory board index

--- Proyecto/js/board.js
+++ Proyecto/js/board.js
@@ -5,9 +5,11 @@ function inicializarTablero() {
     console.log(` [board.js] Generando el tablero para la dificultad: ${dificultadSeleccionada}`);
 
     if (dificultadSeleccionada === "easy") {
+        
 
     } else if (dificultadSeleccionada === "medium") {
 
     } else if (dificultadSeleccionada === "hard") {
     }
 }
+
